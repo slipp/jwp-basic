79af6f272f014374b645c96aabe7bef744267c21
javajigi <javajigi@gmail.com>
2016-02-04 16:16:04 +0900

    ajax를 위한 기반 작업 진행함.

--- webapp/js/scripts.js
+++ webapp/js/scripts.js
@@ -1,2 +1,10 @@
-
+String.prototype.format = function() {
+  var args = arguments;
+  return this.replace(/{(\d+)}/g, function(match, number) {
+    return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+        ;
+  });
+};
 
