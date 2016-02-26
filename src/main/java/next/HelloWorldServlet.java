59651416b3794e15c63887d7c70ebf9ba7a9e22a
javajigi <javajigi@gmail.com>
2016-02-26 14:59:40 +0900

    Hello World를 출력하는 Servlet 추가함.

---
+++ src/main/java/next/HelloWorldServlet.java
@@ -0,0 +1,19 @@
+package next;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.print("Hello World!");
+	}
+}
