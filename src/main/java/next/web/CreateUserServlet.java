3bfd92c4b58ec3096985275fd69feb5939a28aa4
javajigi <javajigi@gmail.com>
2016-02-26 17:24:30 +0900

    회원가입/사용자 목록 기능 개선함.

--- src/main/java/next/web/CreateUserServlet.java
+++ src/main/java/next/web/CreateUserServlet.java
@@ -2,19 +2,23 @@ package next.web;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import core.db.DataBase;
 import next.model.User;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import core.db.DataBase;
 
 @WebServlet("/user/create")
 public class CreateUserServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+	private static final Logger log = LoggerFactory.getLogger(CreateUserServlet.class);
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -23,12 +27,8 @@ public class CreateUserServlet extends HttpServlet {
                 req.getParameter("password"), 
                 req.getParameter("name"),
                 req.getParameter("email"));
-        System.out.println("User : " + user);
-
+        log.debug("user : {}", user);
         DataBase.addUser(user);
-
-        req.setAttribute("users", DataBase.findAll());
-        RequestDispatcher rd = req.getRequestDispatcher("/user/list.jsp");
-        rd.forward(req, resp);
+        resp.sendRedirect("/user/list");
     }
 }
