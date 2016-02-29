c6ee010606d807a4ef51b0aaed22dca16d545349
javajigi <javajigi@gmail.com>
2016-02-29 16:13:35 +0900

    Q&A SQL 추가 및 DB 초기화 추가함.

--- src/test/java/next/dao/UserDaoTest.java
+++ src/test/java/next/dao/UserDaoTest.java
@@ -1,12 +1,23 @@
 package next.dao;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
+import static org.junit.Assert.assertEquals;
 import next.model.User;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import core.jdbc.ConnectionManager;
+
 public class UserDaoTest {
+	@Before
+	public void setup() {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("jwp.sql"));
+		DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+	}
 
 	@Test
 	public void crud() throws Exception {
