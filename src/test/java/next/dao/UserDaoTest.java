9e31ba34791b0047f4f69e03e3816573276b139a
javajigi <javajigi@gmail.com>
2016-02-29 16:11:19 +0900

    JUnit 실행시 jwp.sql 파일을 읽어 초기화함.

--- src/test/java/next/dao/UserDaoTest.java
+++ src/test/java/next/dao/UserDaoTest.java
@@ -1,19 +1,30 @@
 package next.dao;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
+import static org.junit.Assert.assertEquals;
 import next.model.User;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import core.jdbc.ConnectionManager;
+
 public class UserDaoTest {
+	@Before
+	public void setup() {
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("jwp.sql"));
+		DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+	}
 
 	@Test
 	public void crud() throws Exception {
 		User expected = new User("userId", "password", "name", "javajigi@email.com");
 		UserDao userDao = new UserDao();
 		userDao.insert(expected);
-		
+
 		User actual = userDao.findByUserId(expected.getUserId());
 		assertEquals(expected, actual);
 	}
