4f3aa8d29a2ccc1f7aeb9fe57f1890233bf7f009
javajigi <javajigi@gmail.com>
2016-03-04 12:03:16 +0900

    MVC 프레임워크 기반으로 동작하도록 리팩토링함.

--- src/main/java/next/controller/qna/ShowController.java
+++ src/main/java/next/controller/qna/ShowController.java
@@ -1,30 +1,20 @@
 package next.controller.qna;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import core.mvc.Controller;
 import next.dao.AnswerDao;
 import next.dao.QuestionDao;
 
-@WebServlet("/qna/show")
-public class ShowController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Long questionId = Long.parseLong(req.getParameter("questionId"));
-        QuestionDao questionDao = new QuestionDao();
-        AnswerDao answerDao = new AnswerDao();
-        req.setAttribute("question", questionDao.findById(questionId));
-        req.setAttribute("answers", answerDao.findAllByQuestionId(questionId));
-        
-        RequestDispatcher rd = req.getRequestDispatcher("/qna/show.jsp");
-        rd.forward(req, resp);
-    }
+public class ShowController implements Controller {
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		Long questionId = Long.parseLong(req.getParameter("questionId"));
+		QuestionDao questionDao = new QuestionDao();
+		AnswerDao answerDao = new AnswerDao();
+		req.setAttribute("question", questionDao.findById(questionId));
+		req.setAttribute("answers", answerDao.findAllByQuestionId(questionId));
+		return "/qna/show.jsp";
+	}
 }
--- src/main/java/next/controller/user/LogoutController.java
+++ src/main/java/next/controller/user/LogoutController.java
@@ -1,24 +1,17 @@
 package next.controller.user;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import core.mvc.Controller;
 import next.controller.UserSessionUtils;
 
-@WebServlet("/users/logout")
-public class LogoutController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        HttpSession session = req.getSession();
+public class LogoutController implements Controller {
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		HttpSession session = req.getSession();
         session.removeAttribute(UserSessionUtils.USER_SESSION_KEY);
-        resp.sendRedirect("/");
-    }
+        return "redirect:/";
+	}
 }
