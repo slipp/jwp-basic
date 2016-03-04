afaf598189b07b505539b45c20054397ada74064
javajigi <javajigi@gmail.com>
2016-03-04 10:45:11 +0900

    js, css, image 처리를 담당하는 서블릿 필터 추가함.

--- src/main/java/core/db/DataBase.java
+++ src/main/java/core/db/DataBase.java
@@ -9,6 +9,9 @@ import next.model.User;
 
 public class DataBase {
 	private static Map<String, User> users = Maps.newHashMap();
+	static {
+		users.put("admin", new User("admin", "password", "자바지기", "admin@slipp.net"));
+	}
 	
 	public static void addUser(User user) {
 		users.put(user.getUserId(), user);
