f96fbc1594e5f637554bb97af6e14b8833433f4c
javajigi <javajigi@gmail.com>
2016-03-18 10:51:13 +0900

    ServletContainerInitializer 관련 클래스 추가함.

---
+++ src/main/java/core/web/WebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package core.web;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public interface WebApplicationInitializer {
+    void onStartup(ServletContext servletContext) throws ServletException;
+}
