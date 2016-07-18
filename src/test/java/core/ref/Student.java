d076ccbb77e1009d9ce992571e52816cf63973f8
javajigi <javajigi@gmail.com>
2016-07-18 15:39:43 +0900

    자바 reflection 예제를 위해 Student 클래스 추가함.

---
+++ src/test/java/core/ref/Student.java
@@ -0,0 +1,15 @@
+package core.ref;
+
+public class Student {
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+}
