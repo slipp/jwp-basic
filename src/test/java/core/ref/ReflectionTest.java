60713c2daa95c1618dadcfaea5437e6a9eedd739
javajigi <javajigi@gmail.com>
2016-07-19 12:12:17 +0900

    2가지 테스트 케이스를 추가함.

--- src/test/java/core/ref/ReflectionTest.java
+++ src/test/java/core/ref/ReflectionTest.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import next.model.Question;
+import next.model.User;
 
 public class ReflectionTest {
     private static final Logger logger = LoggerFactory.getLogger(ReflectionTest.class);
@@ -12,6 +13,18 @@ public class ReflectionTest {
     @Test
     public void showClass() {
         Class<Question> clazz = Question.class;
+        logger.debug(clazz.getName());
+    }
+    
+    @Test
+    public void newInstanceWithConstructorArgs() {
+        Class<User> clazz = User.class;
+        logger.debug(clazz.getName());
+    }
+    
+    @Test
+    public void privateFieldAccess() {
+        Class<Student> clazz = Student.class;
         logger.debug(clazz.getName());
     }
 }
