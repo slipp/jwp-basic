ca18595db8d2960c441bf081e7c9387fffe4f1ed
HeewonChae <korman2444@gmail.com>
2016-11-01 07:05:49 +0900

    mvc 패턴 적용

--- src/main/java/next/controller/CreateUserController.java
+++ src/main/java/next/controller/CreateUserController.java
@@ -1,39 +1,18 @@
 package next.controller;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import core.db.DataBase;
 import next.model.User;
 
-@WebServlet(value = { "/users/create", "/users/form" })
-public class CreateUserController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-    private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
+public class CreateUserController implements Controller {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        RequestDispatcher rd = req.getRequestDispatcher("/user/form.jsp");
-        rd.forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		User user = new User(req.getParameter("userId"), req.getParameter("password"), req.getParameter("name"),
                 req.getParameter("email"));
-        log.debug("User : {}", user);
-
-        DataBase.addUser(user);
-
-        resp.sendRedirect("/");
-    }
+		DataBase.addUser(user);
+		return "redirect:/";
+	}
 }
--- src/main/java/next/controller/LoginController.java
+++ src/main/java/next/controller/LoginController.java
@@ -1,11 +1,5 @@
 package next.controller;
 
-import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -13,39 +7,25 @@ import javax.servlet.http.HttpSession;
 import core.db.DataBase;
 import next.model.User;
 
-@WebServlet(value = { "/users/login", "/users/loginForm" })
-public class LoginController extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+public class LoginController implements Controller {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        forward("/user/login.jsp", req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String userId = req.getParameter("userId");
+	@Override
+	public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		String userId = req.getParameter("userId");
         String password = req.getParameter("password");
         User user = DataBase.findUserById(userId);
         if (user == null) {
             req.setAttribute("loginFailed", true);
-            forward("/user/login.jsp", req, resp);
-            return;
+            return "/user/login.jsp";
         }
 
         if (user.matchPassword(password)) {
             HttpSession session = req.getSession();
             session.setAttribute(UserSessionUtils.USER_SESSION_KEY, user);
-            resp.sendRedirect("/");
+            return "redirect:/";
         } else {
             req.setAttribute("loginFailed", true);
-            forward("/user/login.jsp", req, resp);
+            return "/user/login.jsp";
         }
-    }
-
-    private void forward(String forwardUrl, HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        RequestDispatcher rd = req.getRequestDispatcher(forwardUrl);
-        rd.forward(req, resp);
-    }
+	}
 }
